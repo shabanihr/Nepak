fac26425144615cce3af958e227ac77aa366ccc1
Hamid Shabani <shabani@irankavehsaipa.com>
2020-07-02 08:39:31 +0430

    Add DbContext and seting startup

---
+++ Nepak.Datalayer/Context/NepakContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Nepak.Datalayer.Context
+{
+    public class NepakContext:DbContext
+    {
+        public NepakContext(DbContextOptions<NepakContext> options) : base(options)
+        {
+
+            
+        }
+    }
+}
--- Nepak.Web/Startup.cs
+++ Nepak.Web/Startup.cs
@@ -5,17 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Nepak.Datalayer.Context;
 
 namespace Nepak.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            #region DataBase Context
 
+            services.AddDbContext<NepakContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("NepakConnection"));
+                }
+            );
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +43,9 @@ namespace Nepak.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
